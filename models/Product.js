695772de42e1d0a58dedd5e18c735a85b9e6d800
Tyler Silverman <silverman.tyler@gmail.com>
2020-12-15 12:14:50 -0500

    edited files, and have excuting in the terminal

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   primaryKey: true,
+    //   autoIncrement: true,
+    // },
+    // product_name: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   varchar: true,
+    //   defaultValue: true,
+    // },
+    // price: {
+    //   type: DataTypes.DECIMAL,
+    //   allowNull: false,
+    // },
+    // stock: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   defaultValue: true
+    // },
+    // category_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     key: 'id'
+    //   },
   },
   {
     sequelize,
